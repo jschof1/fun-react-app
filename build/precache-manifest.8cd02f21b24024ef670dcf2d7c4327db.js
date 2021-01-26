a1951bcb6cf91587d4cfde33edace89f0ba64111
Jack Schofield <59843981+jschof1@users.noreply.github.com>
2021-01-26 12:13:59 -0400

    fix

---
+++ build/precache-manifest.8cd02f21b24024ef670dcf2d7c4327db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7643259127787375a0460a4dc24a757a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cad14fad2157b8c637c2",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "801d1df86005a307d468",
+    "url": "/static/css/main.e73bfb37.chunk.css"
+  },
+  {
+    "revision": "cad14fad2157b8c637c2",
+    "url": "/static/js/2.e81ab978.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.e81ab978.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "801d1df86005a307d468",
+    "url": "/static/js/main.75c3ba9e.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
